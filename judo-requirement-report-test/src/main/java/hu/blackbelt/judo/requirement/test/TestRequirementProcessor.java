e40c13d4974f60356764d2d1d1592eb488758097
Ferenc Magnucz <ferenc.magnucz@blackbelt.hu>
2022-12-21 17:51:10 +0100

    TestRequirementProcessor skeleton has been refactored.

--- judo-requirement-report-test/src/main/java/hu/blackbelt/judo/requirement/test/TestRequirementProcessor.java
+++ judo-requirement-report-test/src/main/java/hu/blackbelt/judo/requirement/test/TestRequirementProcessor.java
@@ -8,8 +8,15 @@ import org.junit.jupiter.api.Test;
 public class TestRequirementProcessor {
 
     @Requirement(reqs = {
-            "aaa",
-            "bbb"
+
+    })
+    @Test
+    public void test00(){
+
+    }
+
+    @Requirement(reqs = {
+            "R01"
     })
     @Test
     public void test01(){
@@ -17,10 +24,36 @@ public class TestRequirementProcessor {
     }
 
     @Requirement(reqs = {
-            "aaa"
+            "R02"
     })
     @Test
     public void test02(){
+
+    }
+
+    @Requirement(reqs = {
+            "R01",
+            "R03"
+    })
+    @Test
+    public void test03(){
+
+    }
+
+    @Requirement(reqs = {
+            "R04",
+            "R05"
+    })
+    @Test
+    public void test04(){
+
+    }
+
+    @Requirement(reqs = {
+            "R01",
+            "R03"
+    })
+    public void test05(){
 
     }
 
