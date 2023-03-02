64c7894daafd7d900126eeb0c9da78a4d7260617
fmagnucz <ferenc.magnucz@blackbelt.hu>
2023-03-02 11:16:43 +0100

    JNG-4570 Create TestCase annotation (#6)
    * JNG-4570 TestCase annotation has been created.
    
    * JNG-4570 RequirementProcessor already processes the TestCase annotation too.
    
    * JNG-4570 pom.xml has been changed. reportPath has to point on a directory.

---
+++ judo-requirement-report-processor/src/main/java/hu/blackbelt/judo/requirement/report/processor/RequirementReportRow.java
@@ -0,0 +1,25 @@
+package hu.blackbelt.judo.requirement.report.processor;
+
+import java.util.Objects;
+
+/**
+ * This class represent a row in the requirement-report.csv. 
+ *
+ */
+class RequirementReportRow {
+    private String testMethod;
+    private String status;
+    private String reqId;
+    private String testCaseId;
+
+    public RequirementReportRow(String testMethod, String testCaseId, String status, String reqId) {
+        this.testMethod = testMethod;
+        this.status = status;
+        this.reqId = reqId;
+        this.testCaseId = testCaseId;
+    }
+
+    String[] toRequirementReportRowStringArray() {
+        return new String[]{testMethod, testCaseId, status, Objects.requireNonNullElse(reqId,"")};
+    }
+}
